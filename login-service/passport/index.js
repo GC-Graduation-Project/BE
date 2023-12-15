7bf1b62c45727d965b6d90776a462b3370eff1cf
hanseul37 <hanseul37@daum.net>
2023-12-15 17:38:24 +0900

    [FEAT] 로그인 구현

---
+++ login-service/passport/index.js
@@ -0,0 +1,22 @@
+const passport = require("passport");
+const local = require("./localStrategy");
+const kakao = require("./kakaoStrategy");
+const User = require("../models/user");
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    //user == exUser
+    done(null, user.id); //유저 id만 추출
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      where: { id },
+    })
+      .then((user) => done(null, user))
+      .catch((err) => done(err));
+  });
+
+  local();
+  kakao();
+};
--- login-service/routes/index.js
+++ login-service/routes/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
-
+const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
 const { test } = require("../controllers");
 
 const router = express.Router();
+
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
 router.get("/test", test);
 
