63e45b4d7c475185b89336474cb1dbc90ad3ee3b
hanseul37 <hanseul37@daum.net>
2023-12-17 01:54:25 +0900

    [FIX] tab-service encoding

--- tab-service/routes/index.js
+++ tab-service/routes/index.js
@@ -1,12 +1,10 @@
 import express from "express";
-import multer from "multer";
-import path from "path";
-import fs from "fs";
 
-import { maketab } from "../controllers/index.js";
+import { maketab, test } from "../controllers/index.js";
 
 const router = express.Router();
 
 router.post("/getSVG", maketab);
+router.post("/test", test);
 
 export default router;
